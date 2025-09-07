ed87b1424d6901646476229cf6ff91524dbd7940
Bryan Joel Cabello Mero <bjcabello55@gmail.com>
2025-09-07 15:14:18 -0500

    manejando por separado el main de las routes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from fastapi import FastAPI, Body
-from fastapi.responses import HTMLResponse, JSONResponse
-from models.movie import Movie, MovieUpdate, MovieCreate
+from fastapi import FastAPI
+from routes import movie
 
 
 app = FastAPI()
@@ -8,56 +7,4 @@ app = FastAPI()
 app.title = "Nuevo titulo"
 app.version = "0.0.1"
 
-movies: list[Movie] = []  
-
-@app.get("/", tags=["Home"])
-def home():
-    return "home"
-
-@app.get("/movies", tags=["Movies"])
-def get_movie()-> list[Movie]:
-    content = [movie.model_dump() for movie in movies]
-    return JSONResponse(content=content)
-
-@app.get("/movies/{id}", tags=["Movies"])
-def get_movie(id: int) -> Movie | dict:
-    for movie in movies:
-        if movie.id == id:
-            return JSONResponse(content=movie.model_dump())
-    return JSONResponse(content={"detail": "Movie not found"}, status_code=404)
-   
-
-@app.get("/movies/", tags=["Movies"])
-def get_movie_by_category(category: str, year: int)-> Movie:
-    for movie in movies:
-        if movie ['category'] == category:
-            content = [movie.model_dump() for movie in movies]
-    return JSONResponse(content=content)
-    
-
-@app.post("/movies", tags=["Movies"])
-def create_movie(movie: MovieCreate) -> list[Movie]:
-    movies.append(movie)
-    content = [movie.model_dump() for movie in movies]
-    return JSONResponse(content=content)
-
-@app.put("/movies/{id}", tags=["Movies"])
-def update_movie(id: int, movie: MovieUpdate) -> list[Movie]:
-    for item in movies:
-        if item.id == id:
-            item.title = movie.title
-            item.overview = movie.overview
-            item.year = movie.year
-            item.category = movie.category
-    content = [movie.model_dump() for movie in movies]
-    return JSONResponse(content=content)
-
-@app.delete("/movies/{id}", tags=["Movies"])
-def delete_movie(id: int) -> list[Movie]:
-    for movie in movies:
-        if movie.id == id:
-            movies.remove(movie)
-            return JSONResponse(content={"message": "Se elimino la pelicula"})
-            
-    content = [movie.model_dump() for movie in movies]
-    return JSONResponse(content=content)
+app.include_router(movie.router)
---
+++ routes/movie.py
@@ -0,0 +1,64 @@
+from fastapi import APIRouter
+from fastapi.responses import HTMLResponse, JSONResponse, PlainTextResponse, RedirectResponse, FileResponse
+from models.movie import Movie, MovieUpdate, MovieCreate
+
+router = APIRouter()
+
+movies: list[Movie] = []  
+
+@router.get("/", tags=["Home"])
+def home():
+    return PlainTextResponse(content="Bienvenido a mi API", status_code=200)
+
+@router.get("/movies", tags=["Movies"])
+def get_movie()-> list[Movie]:
+    content = [movie.model_dump() for movie in movies]
+    return JSONResponse(content=content, status_code=200)
+
+@router.get("/movies/{id}", tags=["Movies"])
+def get_movie(id: int) -> Movie | dict:
+    for movie in movies:
+        if movie.id == id:
+            return JSONResponse(content=movie.model_dump())
+    return JSONResponse(content={}, status_code=404)
+   
+
+@router.get("/movies/", tags=["Movies"])
+def get_movie_by_category(category: str, year: int)-> Movie:
+    for movie in movies:
+        if movie ['category'] == category:
+            content = [movie.model_dump() for movie in movies]
+    return JSONResponse(content=content, status_code=404)
+    
+
+@router.post("/movies", tags=["Movies"])
+def create_movie(movie: MovieCreate) -> list[Movie]:
+    movies.append(movie)
+    content = [movie.model_dump() for movie in movies]
+    return JSONResponse(content=content,  status_code=201)
+    #return RedirectResponse(url="/movies", status_code=303)
+
+@router.put("/movies/{id}", tags=["Movies"])
+def update_movie(id: int, movie: MovieUpdate) -> list[Movie]:
+    for item in movies:
+        if item.id == id:
+            item.title = movie.title
+            item.overview = movie.overview
+            item.year = movie.year
+            item.category = movie.category
+    content = [movie.model_dump() for movie in movies]
+    return JSONResponse(content=content, status_code=200)
+
+@router.delete("/movies/{id}", tags=["Movies"])
+def delete_movie(id: int) -> list[Movie]:
+    for movie in movies:
+        if movie.id == id:
+            movies.remove(movie)
+            return JSONResponse(content={"message": "Se elimino la pelicula"})
+            
+    content = [movie.model_dump() for movie in movies]
+    return JSONResponse(content=content, status_code=200)
+
+# @app.get("/file")
+# def get_file():
+#     return FileResponse('requirements.txt')
