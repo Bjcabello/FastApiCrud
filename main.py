70636814ec1d9779b50cbab5009516f0e8272ca3
Bryan Joel Cabello Mero <bjcabello55@gmail.com>
2025-09-07 12:41:04 -0500

    corrigiendo crud

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Body
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, JSONResponse
 from models.movie import Movie, MovieUpdate, MovieCreate
 
 
@@ -16,41 +16,48 @@ def home():
 
 @app.get("/movies", tags=["Movies"])
 def get_movie()-> list[Movie]:
-    return [movie.model_dump() for movie in movies]
+    content = [movie.model_dump() for movie in movies]
+    return JSONResponse(content=content)
 
 @app.get("/movies/{id}", tags=["Movies"])
-def get_movie(id: int)-> Movie:
+def get_movie(id: int) -> Movie | dict:
     for movie in movies:
-        if movie ['id'] == id:
-            return movie.model_dump()
-    return []
+        if movie.id == id:
+            return JSONResponse(content=movie.model_dump())
+    return JSONResponse(content={"detail": "Movie not found"}, status_code=404)
+   
 
 @app.get("/movies/", tags=["Movies"])
 def get_movie_by_category(category: str, year: int)-> Movie:
     for movie in movies:
         if movie ['category'] == category:
-            return movie.model_dump()
-    return []
+            content = [movie.model_dump() for movie in movies]
+    return JSONResponse(content=content)
+    
 
 @app.post("/movies", tags=["Movies"])
 def create_movie(movie: MovieCreate) -> list[Movie]:
     movies.append(movie)
-    return [movie.model_dump() for movie in movies]
+    content = [movie.model_dump() for movie in movies]
+    return JSONResponse(content=content)
 
 @app.put("/movies/{id}", tags=["Movies"])
 def update_movie(id: int, movie: MovieUpdate) -> list[Movie]:
     for item in movies:
-        if item['id'] == id:
-            item['title'] = movie.title
-            item['overview'] = movie.overview
-            item['year'] = movie.year
-            item['category'] = movie.category
-    return [movie.model_dump() for movie in movies]
+        if item.id == id:
+            item.title = movie.title
+            item.overview = movie.overview
+            item.year = movie.year
+            item.category = movie.category
+    content = [movie.model_dump() for movie in movies]
+    return JSONResponse(content=content)
 
 @app.delete("/movies/{id}", tags=["Movies"])
-def delete_movie(id: int)-> list[Movie]:
+def delete_movie(id: int) -> list[Movie]:
     for movie in movies:
-        if movie['id'] == id:
+        if movie.id == id:
             movies.remove(movie)
-            return {"message": f"Pelicula '{movie['title']}' eliminada exitosamente"}
-    return [movie.model_dump() for movie in movies]
+            return JSONResponse(content={"message": "Se elimino la pelicula"})
+            
+    content = [movie.model_dump() for movie in movies]
+    return JSONResponse(content=content)
