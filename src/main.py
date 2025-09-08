d188c187ff16e8c08d76a47008d444323c83a221
Bryan Joel Cabello Mero <bjcabello55@gmail.com>
2025-09-07 19:45:33 -0500

    modificando middlewares

--- src/main.py
+++ src/main.py
@@ -5,6 +5,9 @@ from src.utils.htttp_error_handler import HttpErrorHandler
 app = FastAPI()
 app.add_middleware(HttpErrorHandler)
 
+   
+    
+
 app.title = "Mi Crud de peliculas"
 app.version = "0.0.1"
 
